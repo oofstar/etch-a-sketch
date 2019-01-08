29c2881d3d682d9e8b127b81bb4651aa03f8f317
Al Fair <thealisonfair@gmail.com>
2019-01-08 16:51:57 -0500

    redraws grid from user specification

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 const container = document.querySelector('#container');
 
-// sets up grid
-for(i = 0; i < 4; i++) {
-  let row = document.createElement('div');
-  row.classList.add("row");
-  container.appendChild(row);
+function createGrid(number) {
+  for(i = 0; i < number; i++) {
+    let row = document.createElement('div');
+    row.classList.add("row");
+    container.appendChild(row);
+  }
+
+  const rows = document.querySelectorAll(".row");
+  rows.forEach((row) => {
+    for (i = 0; i < number; i++) {
+      let boxDiv = document.createElement('div');
+      boxDiv.style.height = `${1200/number}px`;
+      boxDiv.style.width = `${1200/number}px`;
+      boxDiv.classList.add('box');
+      row.appendChild(boxDiv);
+    }
+  });
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.addEventListener('mouseover', (e) => {
+      box.style.backgroundColor = "red";
+    });
+  })
 }
 
-const rows = document.querySelectorAll(".row");
-rows.forEach((row) => {
-  for (i = 0; i < 4; i++) {
-    let boxDiv = document.createElement('div');
-    boxDiv.classList.add('box');
-    row.appendChild(boxDiv);
-  }
-});
+function restart() {
+  console.log("you hit restart");
+  container.innerText = " ";
+  number = prompt(`How many cells do you want in your grid width?`);
+  createGrid(number);
 
-const boxes = document.querySelectorAll(".box");
-boxes.forEach((box) => {
-  box.addEventListener('mouseover', (e) => {
-    box.style.backgroundColor = "red";
-  });
-});
+}
+
+createGrid(16);
+const restartButton = document.querySelector('#restart')
